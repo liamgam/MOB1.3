7f58228eb8ca238a4dc66026ee579b9643cf0fdc
liamgam <liam.liamgamble@gmail.com>
2019-01-31 16:50:28 -0800

    setting data from target actions

---
+++ CommunicationClass/CommunicationClass/LoginButton.swift
@@ -0,0 +1,19 @@
+//
+//  LoginButton.swift
+//  CommunicationClass
+//
+//  Created by Rinni Swift on 1/31/19.
+//  Copyright © 2019 Adriana González Martínez. All rights reserved.
+//
+
+import UIKit
+
+class LoginButton: UIButton {
+
+    var dict = [String: Any]()
+    
+    // OR
+    /*
+    var dicts: Dictionary<String, Any> = [:]
+    */
+}
---
+++ CommunicationClass/CommunicationClass/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CommunicationClass
+//
+//  Created by Adriana González Martínez on 1/31/19.
+//  Copyright © 2019 Adriana González Martínez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+
+    @IBOutlet weak var loginButton: LoginButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
+        loginButton.dict = ["Rinni": 19]
+
+    }
+    
+    @objc func loginButtonPressed(sender: LoginButton){
+        print(sender.dict)
+        
+    }
+}
+
